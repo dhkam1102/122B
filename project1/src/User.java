46e7f51185e670eddf96e60c9626b302ed06ff19
Brian Kam <brianhyunkam@gmail.com>
2024-04-22 15:09:09 -0700

    login page merged with this branch, function all working checked

---
+++ project1/src/User.java
@@ -0,0 +1,47 @@
+/**
+ * This User class only has the username field in this example.
+ * You can add more attributes such as the user's shopping cart items.
+ */
+public class User {
+
+    private final String id;
+    private final String firstName;
+    private final String lastName;
+    private final String ccId;
+    private final String address;
+    private final String email;
+
+    public User(String id, String firstName, String lastName, String ccId, String address, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.ccId = ccId;
+        this.address = address;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCcId() {
+        return ccId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
