70055f290e8b8d4ac7ed2ecb80d0abc9fbb243a7
Brian Kam <brianhyunkam@gmail.com>
2024-05-20 18:19:28 -0700

    fuzzy search

--- project1/src/MovieList.java
+++ project1/src/MovieList.java
@@ -379,6 +379,10 @@ public class MovieList extends HttpServlet {
                             mid_query.append(word);
                         }
                         mid_query.append("' IN BOOLEAN MODE) ");
+
+                        // fuzzy search
+                        mid_query.append("OR ed(m.title, '").append(title).append("') <= 2) ");
+
                     }
 
                     String mid_query_string = mid_query.toString();
@@ -512,9 +516,10 @@ public class MovieList extends HttpServlet {
         JsonArray jsonArray = new JsonArray();
 
         try(Connection conn = dataSource.getConnection()) {
-            String movieQuery = "SELECT id, title FROM movies WHERE MATCH (title) AGAINST (? IN BOOLEAN MODE) LIMIT 10";
+            String movieQuery = "SELECT id, title FROM movies WHERE MATCH (title) AGAINST (? IN BOOLEAN MODE) OR ed(title, ?) <= 2 LIMIT 10";
             try (PreparedStatement statement = conn.prepareStatement(movieQuery)) {
                 statement.setString(1, "+" + query.replace(" ", " +") + "*");
+                statement.setString(2, query);
                 ResultSet rs = statement.executeQuery();
 
                 while (rs.next()) {
